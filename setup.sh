f9c68581099e497ed483b83c7d7b2a85894c07c5
im1986 <mcomein@163.com>
2016-10-11 09:24:07 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+#设置文件权限
 sudo chmod 755 *.so
 sudo chmod 755 mjpg_streamer
+#添加开机启动
 sudo cp -f webcam /etc/init.d/webcam
 sudo chmod 755 /etc/init.d/webcam
 sudo update-rc.d webcam defaults
